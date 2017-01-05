34b28a012787da636beaa188e64dd63946626699
Spencer Barton <sbarton272@gmail.com>
2017-01-04 22:08:25 -0800

    Got basic map working

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, send_from_directory
 from datetime import datetime
 
 app = Flask(__name__, template_folder='assets/templates/')
@@ -8,6 +8,11 @@ def homepage():
     the_time = datetime.now().strftime("%A, %d %b %Y %l:%M %p")
 
     return render_template('index.html', my_string="Wheeeee!", my_list=[0,1,2,3,4,5])
+
+
+@app.route('/assets/js/<path:path>')
+def send_js(path):
+    return send_from_directory('assets/js', path)
 
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True)
