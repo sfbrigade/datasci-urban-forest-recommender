b4901d6dda99f893bb791ac6b7aa70e929572f4d
Spencer Barton <sbarton272@gmail.com>
2017-01-04 20:48:49 -0800

    Working on adding jinja2 templates and basic map

--- app.py
+++ app.py
@@ -1,18 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 from datetime import datetime
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='assets/templates/')
 
 @app.route('/')
 def homepage():
     the_time = datetime.now().strftime("%A, %d %b %Y %l:%M %p")
 
-    return """
-    <h1>Hello heroku</h1>
-    <p>It is currently {time}.</p>
-
-    <img src="http://loremflickr.com/600/400">
-    """.format(time=the_time)
+    return render_template('index.html', my_string="Wheeeee!", my_list=[0,1,2,3,4,5])
 
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True)
