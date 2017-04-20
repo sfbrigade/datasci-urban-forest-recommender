f3e9bc4e7790b7130c74f3fc671113b71bdeedb0
Spencer Barton <sbarton272@gmail.com>
2017-04-19 20:07:05 -0700

    Cleaning up readme and code

--- README.md
+++ README.md
@@ -1,6 +1,25 @@
-# datasci-urban-forest-recommender
-Basic App to Recommend Trees
+# Urban Forest Recommender
+This project is a San Francisco tree recommendation website. Trees are recommended for any location in the city based on the health of the surrounding trees. Tree health data is from the 2016 census and can be found in `assets/data/trees.csv`. In addition to recommending based on tree health, we only recommend trees that the city allows to be planted.
+
+What remains to be done:
+- Make it look nicer
+- Potentially integrate with the [Friends of the Urban Forest website](www.fuf.net/resources-reference/urban-tree-species-directory/)
+
+# Install Locally
+
+1. `virtaulenv venv`
+1. `source venv/bin/activate`
+1. `pip install -r requirements.txt`
+
+# How to Run Locally
+
+- Run `env FLASK_APP=app.py flask run`
+
+# How to Deploy
 
 - Hosted on Heroku
-- Push to `deployment` to deploy
-- Run `env FLASK_APP=app.py flask run`
+- Push to `deployment` branch to deploy:
+  1. `git checkout deployment`
+  1. `git rebase master`
+  1. `git push -f`
+
--- model.py
+++ model.py
@@ -1,8 +1,12 @@
+import os
 import pandas as pd
 from sklearn.neighbors import NearestNeighbors
 
 class TreeRecommendation():
     def __init__(self):
+        print('*'*25)
+        print('INITIALIZING MODEL')
+
         df_invtry_new = pd.read_csv('assets/data/trees.csv')
         # Drop ones without a condition score
         df_tree_cond = df_invtry_new.dropna(subset = ['condition']).copy()
@@ -25,7 +29,7 @@ class TreeRecommendation():
         # Drop the trees that were not mapped
         df_tree_cond = df_tree_cond.dropna(subset = ['condition'])
 
-        # Convert to neg score for poor and positive for goodnp
+        # Convert to neg score for poor and positive for good tree conditions
         df_tree_cond['condition_score'] = df_tree_cond.condition.map({'good': 1, 'fair': 0, 'poor': -1})
 
         self.df_tree_cond = df_tree_cond
@@ -34,14 +38,15 @@ class TreeRecommendation():
         self.knn = NearestNeighbors(algorithm = 'ball_tree').fit(df_tree_cond[['latitude', 'longitude']])
 
         # Load the rules dataset
-        self.df_rules = pd.read_excel('assets/data/sf_tree_rules.xlsx', sheetname = 0)
+        df_rules = pd.read_excel('assets/data/sf_tree_rules.xlsx', sheetname = 0)
+        self.allowed_species = pd.DataFrame(df_rules['species'].apply(lambda x: x.lower()))
+        print('Allowed species:', ', '.join(self.allowed_species['species'].values))
 
     def data_html(self):
         return self.df_tree_cond.to_html()
 
     def recommend(self, latitude, longitude):
         print('Recommending for', latitude, 'lat', longitude, 'long')
-
 
         # Get condition scores across the city
         dists, nearest_trees = self.knn.kneighbors(X = [[latitude, longitude]],
@@ -50,9 +55,10 @@ class TreeRecommendation():
 
         # Keep recommendation spots that are close enough to existing trees
         mean_dist = dists[0].mean()
-        if mean_dist > 0.005:
+        if mean_dist > os.environ.get('MEAN_DISTANCE_CUTOFF', 0.01):
             return ['No-Result', 'Mean distance from nearest trees is {}.'.format(mean_dist)]
 
+        # Select mean coundition of the nearby species
         nearest_trees = nearest_trees[0]
         df_pick = pd.DataFrame()
         local_trees = self.df_tree_cond.iloc[nearest_trees]
@@ -63,10 +69,13 @@ class TreeRecommendation():
                             ascending = False).reset_index()
 
         pick['botanical'] = pick['botanical'].apply(lambda x: x.lower())
-        accepted_species = self.df_rules['species'].apply(lambda x: x.lower()).reset_index(0)
 
-        joined = pd.merge(pick, accepted_species,
+        # Filter down to allowed species
+        joined = pd.merge(pick, self.allowed_species,
                           left_on = 'botanical', right_on = 'species', how = 'inner')
+
+        if len(joined) == 0:
+            return ['No-Result', 'Nearby trees are not allowed to be planted.']
 
         return joined['common'].apply(lambda x: x.lower()).tolist()
 
